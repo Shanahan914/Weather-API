1d4390af3b43c19ab28f31314ebda9cc9da512b8
Martin Shanahan <m.shanahan914@gmail.com>
2024-08-11 22:02:20 +0100

    v0.1 adding proper routing

--- API/app.py
+++ API/app.py
@@ -1,20 +1,25 @@
-from flask import Flask 
+from flask import Flask, request
 import urllib3, json, os
 import certifi
+import redis 
 
 
 app = Flask(__name__)
 
 
+@app.get("/<location>")
+def get_weather(location):
+    print(location)
+    filterstartarg = request.args.get('start')
+    filterendarg=request.args.get('end')
+    filterstart = filterstartarg if filterstartarg else ""
+    filterend = filterendarg if filterendarg else ""
+    url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{start}/{end}/?key={key}'.format(key=os.environ.get("WEATHER_API_KEY"), location=location, start=filterstart, end=filterend)
 
-@app.route("/")
-def get_movies():
-    url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Glasgow/2024-08-09?key={}'.format(os.environ.get("WEATHER_API_KEY"))
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     try:
         response = http.request('GET', url, timeout=5.0)
         print(response.status)
-
         if response.status == 200:
             data = json.loads(response.data.decode('utf-8'))
             return (data)
@@ -26,11 +31,7 @@ def get_movies():
         print(f"Request error occurred: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-    return ("<p> arghh! </p>")
-
-@app.get("/<location>")
-def get_weather(location):
-    pass 
+    return "An error has occurred"
 
 
 
